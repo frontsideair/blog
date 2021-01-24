0d1c3c33a75d321d7522d2e0bc07f17a5c631d94
Fatih Altinok <photonia@gmail.com>
2021-01-24 20:15:37 +0300

    Revert "Merge pull request #30 from frontsideair/quirkiness"
    This reverts commit be57a57a5ef12ca48e59a5e23ff31930b52a2552, reversing
    changes made to b2bde5673328372acf8696867b569e47af449037.

--- src/components/seo.js
+++ src/components/seo.js
@@ -91,7 +91,7 @@ SEO.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string,
+  title: PropTypes.string.isRequired,
 }
 
 export default SEO
