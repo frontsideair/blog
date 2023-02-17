f526cbc614efc76207b7a591ff78a9fd020904a8
Fatih Altinok <fatihaltinok@live.com>
2023-02-17 15:59:34 +0300

    Add links to headings

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          "gatsby-remark-autolink-headers",
           {
             resolve: `gatsby-remark-images`,
             options: {
