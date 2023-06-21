448e62d5244bcdb3b3f49bfd1af4edf119c23c45
Fatih Altinok <fatihaltinok@live.com>
2023-06-22 00:23:40 +0300

    Remove remnants of splitbee integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -136,7 +136,6 @@ module.exports = {
       },
     },
     "gatsby-plugin-sitemap",
-    "gatsby-plugin-zeit-now",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
